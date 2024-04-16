13880ba54ad55e64d7a49b8091c40d9f8c55691c
Vinit Shirbhate <vinit.shirbhate23@vit.edu>
2024-04-16 11:12:13 +0530

    login backend

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,66 +1,81 @@
 const Signup = () => {
-	
-    return (
-		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-			<div className='w-full p-6 rounded-lg shadow-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-				<h1 className='text-3xl font-semibold text-center text-gray-800'>
-					Sign Up
-				</h1>
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-800">
+          Sign Up
+        </h1>
 
-				<form>
-					<div>
-						<label className='label p-2'>
-							<span className='text-base label-text'>Full Name</span>
-						</label>
-						<input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10' />
-					</div>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Full Name "
+              className="w-full input input-bordered  h-10"
+            />
+          </div>
 
-					<div>
-						<label className='label p-2 '>
-							<span className='text-base label-text'>Username</span>
-						</label>
-						<input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
-					</div>
+          <div>
+            <label className="label p-2 ">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
 
-					<div>
-						<label className='label'>
-							<span className='text-base label-text'>Password</span>
-						</label>
-						<input
-							type='password'
-							placeholder='Enter Password'
-							className='w-full input input-bordered h-10'
-						/>
-					</div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
 
-					<div>
-						<label className='label'>
-							<span className='text-base label-text'>Confirm Password</span>
-						</label>
-						<input
-							type='password'
-							placeholder='Confirm Password'
-							className='w-full input input-bordered h-10'
-						/>
-					</div>
+          <div>
+            <label className="label">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
 
-					{/* <GenderCheckbox /> */}
+          {/* <GenderCheckbox /> */}
 
-					<a className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
-						Already have an account?
-					</a>
+          <a
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+            href="#"
+          >
+            Already have an account?
+          </a>
 
-					<div className="flex justify-center items-center mt-2   ">
-                    <button type="button" class="text-black bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-semibold rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">SignUp</button>
-					</div>
-                    {/* <div>
+          <div className="flex justify-center items-center mt-2   ">
+            <button
+              type="button"
+              class="text-black bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-semibold rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              SignUp
+            </button>
+          </div>
+          {/* <div>
 						<button className='btn btn-block btn-sm mt-2 border border-slate-700' >
 							SignUp
 						</button>
 					</div> */}
-				</form>
-			</div>
-		</div>
-	);
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Signup;
